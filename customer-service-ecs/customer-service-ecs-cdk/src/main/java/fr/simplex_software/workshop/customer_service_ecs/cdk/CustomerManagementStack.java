21436f2d15b44b96d47e98d1731d9faaa06b18b4
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2025-11-09 17:31:21 +0100

    Ready to publish

--- customer-service-ecs/customer-service-ecs-cdk/src/main/java/fr/simplex_software/workshop/customer_service_ecs/cdk/CustomerManagementStack.java
+++ customer-service-ecs/customer-service-ecs-cdk/src/main/java/fr/simplex_software/workshop/customer_service_ecs/cdk/CustomerManagementStack.java
@@ -2,54 +2,58 @@ package fr.simplex_software.workshop.customer_service_ecs.cdk;
 
 import jakarta.inject.*;
 import org.eclipse.microprofile.config.inject.*;
+import fr.simplex_software.workshop.customer_service_ecs.cdk.config.InfrastructureConfig;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.*;
-import software.amazon.awscdk.services.elasticache.CfnSubnetGroup;  // Add this import
+import software.amazon.awscdk.services.elasticache.CfnSubnetGroup;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.amazon.awscdk.services.iam.*;
 import software.amazon.awscdk.services.logs.*;
 import software.amazon.awscdk.services.rds.*;
-import software.amazon.awscdk.services.elasticloadbalancingv2.*;
-
 
 import java.util.*;
 
 @Singleton
-public class CustomerManagementStack extends Stack
-{
+public class CustomerManagementStack extends Stack {
+
   @ConfigProperty(name = "cdk.image_name")
   String imageName;
+
   @ConfigProperty(name = "cdk.container.port", defaultValue = "8080")
   int containerPort;
+
+  @Inject
+  InfrastructureConfig config;
+
   @Inject
   public CustomerManagementStack(final App scope,
-    final @ConfigProperty(name = "cdk.stack-id",
-      defaultValue = "QuarkusCustomerManagementStack") String stackId,
-    final StackProps props)
-  {
+    final @ConfigProperty(name = "cdk.stack-id", defaultValue = "QuarkusCustomerManagementStack") String stackId,
+    final StackProps props) {
     super(scope, stackId, props);
+    initStack();
   }
 
-  public void initStack()
-  {
+  public void initStack() {
     Vpc vpc = Vpc.Builder.create(this, "CustomerVpc")
-      .maxAzs(2)
-      .natGateways(1)
+      .maxAzs(config.vpc().maxAzs())
+      .natGateways(config.vpc().natGateways())
       .build();
 
     DatabaseInstance database = DatabaseInstance.Builder.create(this, "CustomerDatabase")
       .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
         .version(PostgresEngineVersion.VER_17_6)
         .build()))
-      .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO))
+      .instanceType(InstanceType.of(
+        InstanceClass.valueOf(config.database().instanceClass()),
+        InstanceSize.valueOf(config.database().instanceSize())))
       .vpc(vpc)
-      .credentials(Credentials.fromGeneratedSecret("postgres"))
-      .databaseName("customers")
-      .deletionProtection(false)
+      .credentials(Credentials.fromGeneratedSecret(config.database().secretUsername()))
+      .databaseName(config.database().databaseName())
+      .deletionProtection(config.database().deletionProtection())
       .removalPolicy(RemovalPolicy.DESTROY)
       .build();
 
@@ -64,56 +68,66 @@ public class CustomerManagementStack extends Stack
       RedisClusterProps.builder()
         .vpc(vpc)
         .subnetGroup(redisSubnetGroup)
-        .clusterId("customer-cache")
-        .description("Redis cache for customer service")
-        .nodeType("cache.t3.micro")
-        .numNodes(1).build());
+        .clusterId(config.redis().clusterId())
+        .description(config.redis().description())
+        .nodeType(config.redis().nodeType())
+        .numNodes(config.redis().numNodes())
+        .build());
 
     Cluster cluster = Cluster.Builder.create(this, "CustomerCluster")
       .vpc(vpc)
       .build();
 
     LogGroup logGroup = LogGroup.Builder.create(this, "CustomerServiceLogGroup")
-      .logGroupName("/ecs/customer-service")
-      .retention(RetentionDays.ONE_WEEK)
+      .logGroupName(config.logging().logGroupName())
+      .retention(RetentionDays.valueOf(config.logging().retentionDays()))
       .build();
 
     ApplicationLoadBalancedFargateService fargateService =
       ApplicationLoadBalancedFargateService.Builder.create(this, "CustomerService")
         .cluster(cluster)
-        .cpu(256)
-        .memoryLimitMiB(512)
-        .desiredCount(2)
+        .cpu(config.ecs().cpu())
+        .memoryLimitMiB(config.ecs().memoryLimitMiB())
+        .desiredCount(config.ecs().desiredCount())
         .taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder()
           .image(ContainerImage.fromRegistry(imageName))
           .containerPort(containerPort)
           .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
             .logGroup(logGroup)
-            .streamPrefix("ecs")
+            .streamPrefix(config.logging().streamPrefix())
             .build()))
           .environment(Map.of(
-            "DB_HOST", database.getInstanceEndpoint().getHostname(),
-            "DB_PORT", "5432",
-            "DB_NAME", "customers",
-            "REDIS_HOST", redis.getPrimaryEndpoint(),
-            "REDIS_PORT", "6379"
+            "QUARKUS_DATASOURCE_JDBC_URL", "jdbc:postgresql://" + database.getInstanceEndpoint().getHostname() + ":5432/" + config.database().databaseName(),
+            "QUARKUS_REDIS_HOSTS", "redis://" + redis.getPrimaryEndpoint() + ":6379"
           ))
           .secrets(Map.of(
-            "DB_USERNAME", Secret.fromSecretsManager(database.getSecret(), "username"),
-            "DB_PASSWORD", Secret.fromSecretsManager(database.getSecret(), "password")
+            "QUARKUS_DATASOURCE_USERNAME", Secret.fromSecretsManager(database.getSecret(), "username"),
+            "QUARKUS_DATASOURCE_PASSWORD", Secret.fromSecretsManager(database.getSecret(), "password")
           ))
           .build())
         .publicLoadBalancer(true)
-        .healthCheckGracePeriod(Duration.seconds(60))
-        .serviceName("customer-service")
+        .healthCheckGracePeriod(Duration.seconds(config.ecs().healthCheckGracePeriodSeconds()))
+        .serviceName(config.ecs().serviceName())
         .build();
+
     fargateService.getTargetGroup()
       .configureHealthCheck(HealthCheck.builder()
-      .path("/q/health")
-      .build());
+        .path("/q/health")
+        .build());
+
     fargateService.getTaskDefinition().getExecutionRole().addManagedPolicy(
       ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonECSTaskExecutionRolePolicy"));
+
     database.getConnections().allowFrom(fargateService.getService(), Port.tcp(5432));
     redis.allowConnectionsFrom(fargateService.getService());
+
+    CfnOutput.Builder.create(this, "CustomerServiceLoadBalancerDNS")
+      .value(fargateService.getLoadBalancer().getLoadBalancerDnsName())
+      .exportName("CustomerServiceLoadBalancerDNS")
+      .build();
+
+    CfnOutput.Builder.create(this, "DatabaseEndpoint")
+      .value(database.getInstanceEndpoint().getHostname())
+      .build();
   }
 }
